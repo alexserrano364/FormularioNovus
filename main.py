4dcec7e57f6b8c42ff320216852669621dd0e588
alexserrano <alexserrano0805@gmail.com>
2022-08-26 04:31:23 -0500

    Se agregó folio, validación y limpieza de datos y se cambió la imagen.

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import os.path
 from tkinter import *
 from tkinter import messagebox
 from tkinter import font
+from PIL import ImageTk, Image
 
 meses = {1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
          9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"}
 
 blue = "#686bb0"
+
 
 def obtenerHora():
     today = datetime.now()
@@ -29,7 +31,7 @@ def obtenerFecha():
 
 def prepararEncabezado(sheet):
     # Hace que toda la primera fila sea negritas
-    header = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"]
+    header = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1"]
     for cell in header:
         activeCell = sheet[cell]
         activeCell.font = Font(bold=True)
@@ -39,8 +41,8 @@ def prepararEncabezado(sheet):
     sheet['D1'] = "Código Postal"
     sheet['E1'] = "Servicio"
     sheet['F1'] = "Importe"
-    sheet['G1'] = "Turno"
-    sheet['H1'] = "Hora"
+    sheet['G1'] = "Hora"
+    sheet['H1'] = "Folio"
 
 
 def conseguirDatos(botones):
@@ -48,51 +50,100 @@ def conseguirDatos(botones):
     for boton in botones:           # Recorre todos los botones y los convierte en texto
         dato = boton.get()
         datos.append(dato)
+    print(datos)
     return datos
 
 
+def datosSonValidos(datos):
+    datos[0] = datos[0].upper()                     # Pasa el nombre a mayúsculas
+
+    datos[1] = datos[1].replace(" ","")             # Se deshace de espacios y guiones en el núm. tel.
+    datos[1] = datos[1].replace("-","")
+
+    datos[4] = datos[4].upper()                     # Pasa el servicio a mayúsculas
+
+    try:                                # Checa que el número telefónico sea un número
+        int(datos[1])
+    except ValueError:
+        messagebox.showinfo("Alerta", "El número telefónico debe ser un número")
+        return False
+
+    try:                                # Checa que el código postal sea un número
+        int(datos[3])
+    except ValueError:
+        messagebox.showinfo("Alerta", "El código postal debe ser un número")
+        return False
+
+    try:                                # Checa que el importe sea un número
+        float(datos[5])
+    except ValueError:
+        messagebox.showinfo("Alerta", "El importe debe ser un número")
+        return False
+    else:
+        datos[5] = "$" + datos[5]
+
+    if datos[6] == "Seleccionar":       # Checa que el turno haya sido seleccionado
+        messagebox.showinfo("Alerta", "Seleccionar Turno")
+        return False
+
+    print(datos)
+    return True
+
+
 def crearExcel(root, botones):
-    # Se obtiene fecha y hora
-    fecha = obtenerFecha()
-    hora = obtenerHora()
-    stringFecha = "%02d/%02d/%02d" % (fecha[0], fecha[1], fecha[2])
-    stringHora = "%02d:%02d" % (hora[0], hora[1])
+    datos = conseguirDatos(botones)
+    if datosSonValidos(datos):                # Solo funciona si los datos están bien
 
-    if 0 <= hora[0] < 7:  # Si es turno de madrugada, se añade a la base del día anterior
-        fecha[0] -= 1
+        # Se obtiene fecha y hora
+        fecha = obtenerFecha()
+        hora = obtenerHora()
+        stringFecha = "%02d/%02d/%02d" % (fecha[0], fecha[1], fecha[2])
+        stringHora = "%02d:%02d" % (hora[0], hora[1])
+        turno = datos[6][:1]
 
-    # Se crea el Excel del día, si ya existe solo se carga
-    title = 'Corte Caja %d de %s del %d.xlsx' % (fecha[0], meses[fecha[1]], fecha[2])
-    if os.path.exists(title):
+
+        if 0 <= hora[0] < 7:  # Si es turno de madrugada, se añade a la base del día anterior
+            fecha[0] -= 1
+
+        # Se crea el Excel del turno, si ya existe solo se carga
+        title = 'Corte Caja %d de %s del %d - %s.xlsx' % (fecha[0], meses[fecha[1]], fecha[2], turno)
+        if os.path.exists(title):
+            workbook = load_workbook(title)
+            sheet = workbook.active
+        else:
+            seriado = 1
+            workbook = openpyxl.Workbook()
+            sheet = workbook.active
+            prepararEncabezado(sheet)
+
+        seriado = len(sheet['H'])
+        folio = "%03d-%04d%02d%02d%s" % (seriado, fecha[2], fecha[1], fecha[0], turno)
+
+        # Se pide y añade la info a la base del turno
+        datos.remove(datos[6])
+        datos.append(stringHora)
+        datos.append(folio)
+        sheet.append(datos)
+        workbook.save(title)
+        workbook.close()
+
+        # Se añade la info a la base general que ya existe
+        title = "Base General.xlsx"
         workbook = load_workbook(title)
         sheet = workbook.active
-    else:
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
-        prepararEncabezado(sheet)
+        datos.append(stringFecha)
+        sheet.append(datos)
+        workbook.save(title)
+        workbook.close()
 
-    datos = conseguirDatos(botones)
-
-    # Se pide y añade la info a la base del día
-    datos.append(stringHora)
-    sheet.append(datos)
-    workbook.save(title)
-    workbook.close()
-
-    # Se añade la info a la base general que ya existe
-    title = "Base General.xlsx"
-    workbook = load_workbook(title)
-    sheet = workbook.active
-    datos.append(stringFecha)
-    sheet.append(datos)
-    workbook.save(title)
-    workbook.close()
-
-    messagebox.showinfo("Éxito", "Datos agregados")
-    root.quit()
+        messagebox.showinfo("Éxito", "Datos agregados")
+        root.quit()
 
 
 def crearInfoVentana(root):
+
+    padx = 65
+    pady = 20
 
     fuenteLabels = font.Font(family="Arial", size=12)
     fuenteMenu = font.Font(family="Arial", size=8)
@@ -124,34 +175,39 @@ def crearInfoVentana(root):
     labelCorreo = Label(root, text="Correo Electrónico:", font=fuenteLabels)
     correo = Entry(root, width=85)
 
+    imgLogo = ImageTk.PhotoImage(Image.open("./img/NovusLogo.jpeg"))
+    labelImagen = Label(image=imgLogo)
+
     botones = [nombre, celular, correo, CP, servicio, importe, turno]
 
-    boton = Button(root, text="Insertar", padx=10, command=lambda: crearExcel(root, botones),
-                   bg=blue, fg="white", font=fuenteBoton)
+    boton = Button(root, text="Insertar", padx=10,
+                   command=lambda: crearExcel(root, botones),
+                   bg=blue, fg="white", font=fuenteBoton, width=30, height=5)
 
     # Insertar labels
-    labelNombre.grid(row=1, column=1, padx=15, pady=5, sticky="W")
-    nombre.grid(row=2, column=1, padx=15, pady=5, sticky="W")
-    labelTurno.grid(row=1, column=3, padx=15, pady=5, sticky="W")
-    turnoMenu.grid(row=2, column=3, padx=15, pady=5, sticky="W")
-    labelServicio.grid(row=4, column=1, padx=15, sticky="W")
-    servicio.grid(row=5, column=1, padx=15, pady=10, sticky="W")
-    labelImporte.grid(row=4, column=3, padx=15, sticky="W")
-    importe.grid(row=5, column=3, padx=15, pady=10, sticky="W")
-    labelCelular.grid(row=7, column=1, padx=15, sticky="W")
-    celular.grid(row=8, column=1, padx=15, pady=10, sticky="W")
-    labelCP.grid(row=7, column=3, padx=15, sticky="W")
-    CP.grid(row=8, column=3, padx=15, pady=10, sticky="W")
-    labelCorreo.grid(row=10, column=1, padx=15, sticky="W")
-    correo.grid(row=11, column=1, padx=15, pady=10, sticky="W")
-    boton.grid(row=12, column=3, padx=15, pady=10)
-
-
+    labelNombre.grid(row=1, column=1, padx=padx, pady=pady, sticky="W")
+    nombre.grid(row=2, column=1, padx=padx, sticky="W")
+    labelTurno.grid(row=1, column=3, padx=padx, pady=pady, sticky="W")
+    turnoMenu.grid(row=2, column=3, padx=padx, sticky="W")
+    labelServicio.grid(row=4, column=1, padx=padx, pady=pady, sticky="W")
+    servicio.grid(row=5, column=1, padx=padx, sticky="W")
+    labelImporte.grid(row=4, column=3, padx=padx, pady=pady, sticky="W")
+    importe.grid(row=5, column=3, padx=padx, sticky="W")
+    labelCelular.grid(row=7, column=1, padx=padx, pady=pady, sticky="W")
+    celular.grid(row=8, column=1, padx=padx, sticky="W")
+    labelCP.grid(row=7, column=3, padx=padx, pady=pady, sticky="W")
+    CP.grid(row=8, column=3, padx=padx, sticky="W")
+    labelCorreo.grid(row=10, column=1, padx=padx, pady=pady, sticky="W")
+    correo.grid(row=11, column=1, padx=padx, sticky="W")
+    boton.grid(row=12, column=3, padx=padx, pady=40)
+    labelImagen.grid(row=12, column=1)
 
 
 def main():
     # Se crea la ventana
     root = Tk()
+    root.title("Sistema de Corte de Caja Novus")
+    root.iconbitmap('./img/caduceus.ico')
     crearInfoVentana(root)
     root.mainloop()
 
